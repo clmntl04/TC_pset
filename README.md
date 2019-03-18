663463ffa45c84bfa547e86025193bf94f6ac96e
Clement Li <clmntl@yahoo.com>
2019-03-18 12:27:09 -0400

    pset

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # TC_pset
+
+Python Library Dependencies:
+tensorflow
+keras
+helpers
+zipfile
+numpy
+os
+sys
+matplotlib.pyplot
+matplotlib.image
+PIL
+skimage.io
+skimage.transform
+__future__
+glob
+
---
+++ utils.py
@@ -0,0 +1,8 @@
+def test_ok():
+    try:
+        from IPython.display import display_html
+        display_html("""<div class="alert alert-success">
+        <strong>Test passed!!</strong>
+        </div>""", raw=True)
+    except:
+        print("test ok!!")
